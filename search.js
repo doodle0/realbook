91c1d01f4863b95751ad80b99050704adfa168b1
doodle0 <49020517+doodle0@users.noreply.github.com>
2021-03-15 15:41:02 +0900

    여러 페이지인 곡 처리, 오류 수정

--- search.js
+++ search.js
@@ -1,13 +1,10 @@
 var data;
 var fuse;
 
-$.getJSON("realbook1.json" , function(data_) {
-    data = data_;
-
-    fuse = new Fuse(data, {
-        keys: ['title', 'page']
-    });
-});
+function initTable() {
+    var tbl = document.getElementById("result-tbl")
+    tbl.innerHTML = '<thead><tr><th width="70%">제목</th><th width="15%">볼륨</th><th>페이지</th></tr></thead>'
+}
 
 function updateSearch() {
     var text = document.getElementById("search-text").value
@@ -15,10 +12,24 @@ function updateSearch() {
     console.log(res)
 
     var tbl = document.getElementById("result-tbl")
-    tbl.innerHTML = '<thead><tr><th width="70%">제목</th><th width="15%">페이지</th><th>링크</th></tr></thead>'
+    initTable();
 
     for (i in res) {
-        if (i >= 20) break;
-        tbl.innerHTML += '<tr><td>' + res[i].item.title + '</td><td>' + res[i].item.page + '</td><td>' + '' + '</td></tr>'
+        if (i >= 15) break;
+        var item = res[i].item;
+        var page_text = item.page_s == item.page_e ? item.page_s : item.page_s + "~" + item.page_e;
+        tbl.innerHTML += '<tr><td>' + item.title + '</td><td>' + item.volume + '</td><td>' + page_text + '</td></tr>'
     }
 }
+
+$.getJSON("realbook1.json" , function(data_) {
+    data = data_;
+
+    fuse = new Fuse(data, {
+        keys: ['title', 'page_s', 'page_e']
+    });
+});
+
+$(function() {
+    initTable();
+});
