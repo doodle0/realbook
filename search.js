b8c6212177f0d3d0e7c86ba57ffb3df4a3075c02
doodle0 <49020517+doodle0@users.noreply.github.com>
2021-10-17 19:29:16 +0900

    검색 페이지 디자인 변경 (Bootstrap)

--- search.js
+++ search.js
@@ -4,32 +4,31 @@ var fuse;
 var searchTopLink;
 
 function initTable() {
-    var tbl = document.getElementById("result-tbl")
-    tbl.innerHTML = '<thead><tr><th width="70%">제목</th><th width="15%">볼륨</th><th width="15%">페이지</th></tr></thead>'
+    let tbl = $("#result-tbl")
+    $('#result-tbl > tbody').empty();
     searchTopLink = null;
 }
 
 function updateSearch() {
-    var text = document.getElementById("search-text").value
-    var res = fuse.search(text)
+    let text = $("#search-text").val()
+    let res = fuse.search(text)
 
-    var tbl = document.getElementById("result-tbl")
     initTable();
 
-    var tbo = document.createElement("tbody");
+    let tbo = $('#result-tbl > tbody');
 
     searchTopLink = null;
 
     for (i in res) {
         if (i >= 15) break;
-        var item = res[i].item;
-        var page_text = item.page_s == item.page_e ? item.page_s : item.page_s + "~" + item.page_e;
-        var link = `view.html?vol=${item.volume}&page_s=${item.page_s}&page_e=${item.page_e}`
-        tbo.innerHTML += `<tr><td><a href="${link}">${item.title}</a></td><td>${item.volume}</td><td>${page_text}</td></tr>`
+        let item = res[i].item;
+        let page_text = item.page_s == item.page_e ? item.page_s : item.page_s + "~" + item.page_e;
+        let link = `view.html?vol=${item.volume}&page_s=${item.page_s}&page_e=${item.page_e}`
+        tbo.append(
+            $(`<tr><td><a href="${link}">${item.title}</a></td><td>${item.volume}</td><td>${page_text}</td></tr>`)
+        );
         if (i == 0) searchTopLink = link;
     }
-
-    tbl.appendChild(tbo);
 }
 
 $(function() {
@@ -49,6 +48,8 @@ function autoScroll() {
         scrollTop: $("#search-text").offset().top
     });
 }
+
+$("#search-text").on("input", updateSearch)
 
 $("#search-text").on("keypress", function(event){
     if (event.keyCode == 13) {
