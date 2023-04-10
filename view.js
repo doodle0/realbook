9ba3cf81f8d6c6cc3efe5ab19e16996251ee8276
doodle0 <49020517+doodle0@users.noreply.github.com>
2023-04-10 20:29:08 +0900

    이미지 로드 중 spinner 표시 추가

--- view.js
+++ view.js
@@ -1,7 +1,24 @@
 const IMG_URL = "https://wypn9z41ir5bzmgjjalyna.on.drv.tw/realbook/rendered"
 
-var autoHideTimer;
+// 이미지 로드 완료 시기 파악
+Image.prototype.load = function(url){
+    var thisImg = this;
+    var xmlHTTP = new XMLHttpRequest();
+    xmlHTTP.open('GET', url,true);
+    xmlHTTP.responseType = 'arraybuffer';
+    xmlHTTP.onload = function(e) {  // 로드 완료 시 placeholder 숨기기
+        var blob = new Blob([this.response]);
+        thisImg.src = window.URL.createObjectURL(blob);
 
+        var thisLoadingPlaceholder = $(thisImg).siblings().filter('.loading-placeholder');
+        thisLoadingPlaceholder.hide();
+    };
+    xmlHTTP.send();
+    
+    return thisImg;
+};
+
+var autoHideTimer;
 function setAutoHideTimer() {
     autoHideTimer = setTimeout(onAutoHideTimeout, 3000);
 }
@@ -33,12 +50,13 @@ $(function () {
     let page_s = 1 * getParam("page_s");
     let page_e = 1 * getParam("page_e");
 
-    let image_view = $('#image-view');
+    let imageView = $('#image-view');
     for (let i = page_s; i <= page_e; i++) {
-        image_view.append(
-            $('<div class="swiper-slide">').append(
-                $(`<img class="fit-to-screen" src="${IMG_URL}/${1000 * vol + i}.jpeg">`)
-            )
+        let img = new Image().load(`${IMG_URL}/${1000 * vol + i}.jpeg`);
+        imageView.append(
+            $('<div class="swiper-slide">')
+                .append($(img).addClass('fit-to-screen'))
+                .append('<div class="spinner-border text-info loading-placeholder role="status"">')
         );
     }
 
